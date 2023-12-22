b4b58d2704e768bf8697d744f9ba33beef1c0640
CodeSandbox <support@codesandbox.io>
2023-12-22 21:53:18 +0000

    Initial commit

--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,10 +1,32 @@
+import Hero from "../components/Hero";
+interface HeroParams {
+  cName: string;
+  heroImg: string;
+  title: string;
+  text: string;
+  url: string;
+  btnClassName: string;
+  buttonText: string;
+}
+
 function About() {
-    return (
-      <>
-        <div></div>
-      </>
-    );
-  }
-  
-  export default About;
-  
+  const objects: HeroParams = {
+    cName: ".hero-mid",
+    heroImg:
+      "https://images.unsplash.com/photo-1531545514256-b1400bc00f31?q=80&w=1074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    title: "About",
+    text: "Choose Your Favourite Destination",
+    url: "/",
+    btnClassName: "hide",
+    buttonText: "Travel Plan",
+  };
+  return (
+    <>
+      <div>
+        <Hero {...objects} />
+      </div>
+    </>
+  );
+}
+
+export default About;
